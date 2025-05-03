b6eedddeb4860db3cd1f7d814ae493461fec82fe
omarcus212 <marcusvinniciusouza@gmail.com>
2025-05-03 12:29:22 -0300

    refactor: routers

--- web/src/routes/PrivateRoute.tsx
+++ web/src/routes/PrivateRoute.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import PageGames from "../pages/games.tsx";
 import PageMyProfile from "../pages/profile";
 import PageUserProfile from "../pages/userProfile/index.tsx";
 import PageHome from "../pages/feed.tsx/index.tsx";
 
 const PrivateRouter: React.FC = () => {
+  const navegate = useNavigate()
   const token = localStorage.getItem("token");
 
-  return token ? (
+  if (!token) {
+    navegate('/home');
+  }
+
+  return (
     <Routes>
       <Route path="/home" element={<PageHome />} />
       <Route path="/games" element={<PageGames />} />
       <Route path="/profile" element={<PageMyProfile />} />
       <Route path="/profile/:username" element={<PageUserProfile />} />
     </Routes>
-  ) : (
-    <Navigate to="/login" replace />
-  );
+  )
 };
 
 export default PrivateRouter;
