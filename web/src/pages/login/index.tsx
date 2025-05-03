3234f3ca4a7b66510ae14f7a488d82c2a3784520
omarcus212 <marcusvinniciusouza@gmail.com>
2025-05-03 15:02:46 -0300

    refactor: router register

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -90,6 +90,10 @@ const PageLogin: React.FC = () => {
     }
   }
 
+  const routeRegister = () => {
+    navigate('/register')
+  }
+
   return (
     <section className="flex flex-wrap justify-center items-center w-full h-screen bg-black">
       <Content className="md:w-1/2 h-full justify-center items-center hidden md:block ">
@@ -107,7 +111,8 @@ const PageLogin: React.FC = () => {
         <ContainerLogin className="bg-white justify-center items-center p-12 rounded-lg gap-4" width="500px" height="600px" text="Sing in">
           <Input label="Email:" type="email" placeholder="Digite seu email..." name="email" onChange={(text: any) => setEmailUserInput(text.target.value)} />
           <Input label="Password:" type="password" placeholder="Digite sua senha..." name="Password" onChange={(text: any) => setPasswordUserInput(text.target.value)} validate={() => ValidatePassword()} />
-          <a href="/register" className="text-black hover:underline" onClick={() => navigate('/register')}>I don't have a registration, Register?</a>
+          <CustomLink text="I don't have a registration, Register?" className="text-black hover:underline" to="/register" onClick={routeRegister} />
+          <a href="/register" className="text-black hover:underline">Register?</a>
           <CustomButton text="Send" className="bg-black w-[231px] h-10 cursor-pointer flex-wrap font-medium text-sm text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none" onClick={onConfirmButtonPress} />
         </ContainerLogin>
       </Content>
